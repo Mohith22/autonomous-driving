eae1e1f902cf93686c5c4ff69e7f2d4ae6a877b6
Mohith22 <mohithdamarapati@gmail.com>
2020-04-18 04:12:46 -0400

    Road Map Not Working

--- code/main.py
+++ code/main.py
@@ -16,9 +16,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+import torch.optim as optim
 
 from data_helper import UnlabeledDataset, LabeledDataset
 from helper import collate_fn, draw_box
+
+from model import Encoder_Decoder #From model.py
 
 random.seed(0)
 np.random.seed(0)
@@ -26,38 +29,86 @@ torch.manual_seed(0)
 
 transform = torchvision.transforms.ToTensor()
 
-image_folder = '../data'
-annotation_csv = '../data/annotation.csv'
 
-train_labeled_scene_index = np.arange(106, 128)
-val_labeled_scene_index = np.arange(128, 134)
+#Load data return data loaders
+def LoadData(image_folder, annotation_csv):
+	train_labeled_scene_index = np.arange(106, 128)
+	val_labeled_scene_index = np.arange(128, 134)
+	labeled_trainset = LabeledDataset(image_folder=image_folder, annotation_file=annotation_csv, 
+		scene_index=train_labeled_scene_index, transform=transform, extra_info=True)
 
-# The labeled dataset can only be retrieved by sample.
-# And all the returned data are tuple of tensors, since bounding boxes may have different size
-# You can choose whether the loader returns the extra_info. It is optional. You don't have to use it.
-labeled_trainset = LabeledDataset(image_folder=image_folder,
-                                  annotation_file=annotation_csv,
-                                  scene_index=train_labeled_scene_index,
-                                  transform=transform,
-                                  extra_info=True
-                                 )
+	labeled_valset = LabeledDataset(image_folder=image_folder, annotation_file=annotation_csv,
+		scene_index=val_labeled_scene_index,transform=transform,extra_info=True)
 
-labeled_valset = LabeledDataset(image_folder=image_folder,
-                                  annotation_file=annotation_csv,
-                                  scene_index=val_labeled_scene_index,
-                                  transform=transform,
-                                  extra_info=True
-                                 )
+	trainloader = torch.utils.data.DataLoader(labeled_trainset, batch_size=2, shuffle=True, num_workers=2, collate_fn=collate_fn)
+	valloader = torch.utils.data.DataLoader(labeled_valset, batch_size=2, shuffle=True, num_workers=2, collate_fn=collate_fn)
 
-trainloader = torch.utils.data.DataLoader(labeled_trainset, batch_size=2, shuffle=True, num_workers=2, collate_fn=collate_fn)
-valloader = torch.utils.data.DataLoader(labeled_valset, batch_size=2, shuffle=True, num_workers=2, collate_fn=collate_fn)
+	return trainloader, valloader
 
-sample, target, road_image, extra = iter(trainloader).next()
-print(target)
+#ThreatScore - Determines Model Performance - Challenge Metric
+def ThreatScore(true, pred):
+	TP = 0
+	FP = 0
+	FN = 0
+	n = len(true)
+	for i in range(n):
+		for j in range(n):
+			if true[i][j] == True and pred[i][j] == True:
+				TP += 1
+			elif true[i][j] == False and pred[i][j] == True:
+				FP += 1
+			elif true[i][j] == True and pred[i][j] == False:
+				FN += 1
+	return TP/(TP+FP+FN)
 
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+def ComputeLoss(criterion, true, pred):
+	loss = 0.0
+	for i in range(800):
+		for j in range(800):
+			loss += criterion(true[:,:,i,j], pred[:,i,j])
+	return loss
 
-num_classes = 9
+def main():
+
+	image_folder = '../data'
+	annotation_csv = '../data/annotation.csv'
+	trainloader, valloader = LoadData(image_folder, annotation_csv)
+	
+	sample, target, road_image, extra = iter(trainloader).next()
+	#print(road_image)
+	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+	#device = "cpu"
+	model = Encoder_Decoder()
+	model.to(device)
+	criterion = nn.BCELoss()
+	optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+	num_epochs = 2
+	
+	for epoch in range(num_epochs):
+		running_loss = 0.0
+		for i, data in enumerate(trainloader, 0):
+			mini_batch_size = len(sample)
+			sample, target, road_image, extra  = data
+			optimizer.zero_grad()
+			outputs = model(torch.stack(sample).to(device))
+			road_image_true = torch.stack([torch.Tensor(x.numpy()) for x in road_image]).to(device)
+			
+			#loss = ComputeLoss(criterion, road_image_true, outputs)
+			#print(road_image_true.size(), outputs.size())
+			
+			loss = criterion(outputs, road_image_true)
+			loss.backward()
+			optimizer.step()
+
+			running_loss += loss.item()
+			if i % mini_batch_size == mini_batch_size-1:
+				print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / mini_batch_size))
+				running_loss = 0.0
+
+
+if __name__ == '__main__':
+	main()
 
 
 
--- code/model.py
+++ code/model.py
@@ -1,9 +1,10 @@
 import torch.nn as nn
 import torch.nn.functional as F
-
-class Encoder(nn.Module):
+import torch
+import copy
+class Encoder_Decoder(nn.Module):
     def __init__(self):
-        super(Encoder, self).__init__()
+        super(Encoder_Decoder, self).__init__()
         #Input Size:- 3 x 256 x 306
         self.encoder_features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=5, padding=(2,3)),
@@ -27,31 +28,43 @@ class Encoder(nn.Module):
             #Current Size:- 192 x 64 x 76
             nn.Conv2d(192, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            #Current Size:- 64s x 64 x 76
+            #Current Size:- 64 x 64 x 76
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             #Current Size:- 64 x 64 x 76
         )
 
-    def forward(self, x):
-        return self.encoder_features(x)
-
-class Decoder(nn.Module):
-    def __init__(self):
-        super(Decoder, self).__init__()
-        #Input Size:- 64 x 64 x 76
         self.decoder_features = nn.Sequential(
-        	nn.Upsample(size=(100,100), mode='bilinear', align_corners=True)
-        	#Current Size:- 64 x 100 x 100
-        	nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1)
-        	#Current Size:- 32 x 200 x 200
-        	nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1)
-        	#Current Size:- 16 x 400 x 400
-        	nn.ConvTranspose2d(16, 1,kernel_size=3, stride=2, padding=1)
-        	#Current Size:- 1 x 800 x 800
+        	nn.Upsample(size=(100,100), mode='bilinear', align_corners=True),
+        	#Current Size:- 384 x 100 x 100
+        	nn.ConvTranspose2d(384, 256, kernel_size=4, stride=2, padding=1),
+        	nn.ReLU(inplace=True),
+        	#Current Size:- 256 x 200 x 200
+        	nn.Conv2d(256, 192, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+        	#Current Size:- 192 x 200 x 200
+        	nn.ConvTranspose2d(192, 64, kernel_size=4, stride=2, padding=1),
+        	nn.ReLU(inplace=True),
+        	#Current Size:- 64 x 400 x 400
+        	nn.Conv2d(64, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            #Current Size:- 32 x 400 x 400
+        	nn.ConvTranspose2d(32, 1,kernel_size=4, stride=2, padding=1),
+        	nn.ReLU(inplace=True),
+        	#Current Size:- 2 x 800 x 800
+        	nn.Sigmoid()
         )
 
     def forward(self, x):
-        return self.decoder_features(x)
+        x = x.permute(1,0,2,3,4)
+        encoder_outs = []
+        for i in range(6):
+            encoder_outs.append(self.encoder_features(x[i]))
+        encoder_output = torch.stack(encoder_outs).permute(0,2,1,3,4)
+        encoder_output = torch.cat([i for i in encoder_output]).permute(1,0,2,3)
+        decoder_output = self.decoder_features(encoder_output)
+        return decoder_output
+
+
 
 
