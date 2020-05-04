a461ce8addc0b7421b3d573c0188e4b4a7af1cfc
orgate <alfredajayaureate@gmail.com>
2020-05-04 05:27:20 -0400

    Minor changes

--- code/Obj-Det/main_depth.py
+++ code/Obj-Det/main_depth.py
@@ -89,7 +89,7 @@ def evaluate(model, valloader, args, criterion):
             sample, target, road_image, extra, depths  = data
             sample_with_depth = torch.cat((torch.stack(sample), torch.stack(depths)), dim=2)
 #            road_image_true = torch.stack([torch.Tensor(x.numpy()) for x in road_image]).to(args.device)
-            target_seg_mask = torch.stack([x for x in target]).to(args.device)
+            target_seg_mask = torch.stack([torch.Tensor(x.numpy()) for x in target]).to(args.device)
             outputs = model(sample_with_depth.to(args.device))
             outputs = torch.squeeze(outputs,dim=1)
             if (args.loss == "both"):
@@ -139,7 +139,7 @@ def main():
             sample, target, road_image, extra, depths  = data
             sample_with_depth = torch.cat((torch.stack(sample), torch.stack(depths)), dim=2)
 #            road_image_true = torch.stack([torch.Tensor(x.numpy()) for x in road_image]).to(args.device)
-            target_seg_mask = torch.stack([x for x in target]).to(args.device)
+            target_seg_mask = torch.stack([torch.Tensor(x.numpy()) for x in target]).to(args.device)
             optimizer.zero_grad()
             outputs = model(sample_with_depth.to(args.device))
             outputs = torch.squeeze(outputs,dim=1)
